18f6c3ad0775d4124d4814f0907a49e545f78ac3
Emre Ozkan <m.e.ozkan@student.tudelft.nl>
2021-06-12 14:01:11 +0200

    asas

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
+    public GameObject old_hand;
     public GameObject hand;
 
     public float xPosLeft;
     public float xPosRight;
+    public float yMin;
+    public float yMax;
+
 
     private bool left;
 
@@ -15,7 +19,32 @@ public class HandController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float yPos = NextFloat(yMin, yMax);
+        old_hand = Instantiate(hand, new Vector3(xPosRight, yPos, 0), Quaternion.identity);
+        left = false;
     }
 
+    static float NextFloat(float min, float max)
+    {
+        System.Random random = new System.Random();
+        double val = (random.NextDouble() * (max - min) + min);
+        return (float)val;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(old_hand);
+        float yPos = NextFloat(yMin, yMax);
+
+        if (left)
+        {
+            left = false;
+            old_hand = Instantiate(hand, new Vector3(xPosRight, yPos, 0), Quaternion.identity);
+        }
+        else
+        {
+            left = true;
+            old_hand = Instantiate(hand, new Vector3(xPosLeft, yPos, 0), Quaternion.identity);
+        }
+    }
 }
