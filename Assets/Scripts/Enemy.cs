dff87524ae066efdc2a6fec0e033f9e27aa44cf8
Emre Ozkan <m.e.ozkan@student.tudelft.nl>
2021-06-13 14:25:26 +0200

    reeee

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float radius;
+    public LayerMask layersToCollide;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +21,36 @@ public class Enemy : MonoBehaviour
 
     void OnCollisionEnter2D (Collision2D col)
     {
+
         if (col.gameObject.tag == "Ash")
         {
             Destroy(col.gameObject);
             Destroy(gameObject);
         }
 
+        if (col.gameObject.tag == "Fire")
+        {
+            GameManager.instance.GameOver();
+        }
+
+    }
+
+    private void HandleCollision()
+    {
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, radius, layersToCollide);
+
+        int c = 0;
+        if (cols.Length > 0)
+        {
+            GameManager.instance.GameOver();
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 
 }
