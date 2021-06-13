3fecfc7e3d362ef9c4fb128e3fd8a9a4105609f3
Efe <e.alkan@student.tudelft.nl>
2021-06-13 12:25:19 +0200

    clouds

--- Assets/Scripts/DragController.cs
+++ Assets/Scripts/DragController.cs
@@ -22,13 +22,13 @@ public class DragController : MonoBehaviour
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
 
-        if (!GameManager.instance.paused)
-        {
-            var pos = Camera.main.WorldToViewportPoint(position);
-            pos.x = Mathf.Clamp01(pos.x);
-            pos.y = Mathf.Clamp01(pos.y);
-            transform.position = Camera.main.ViewportToWorldPoint(pos);   
-        }
+        // if (!GameManager.instance.paused)
+        // {
+        //     var pos = Camera.main.WorldToViewportPoint(position);
+        //     pos.x = Mathf.Clamp01(pos.x);
+        //     pos.y = Mathf.Clamp01(pos.y);
+        //     transform.position = Camera.main.ViewportToWorldPoint(pos);   
+        // }
     }
 
     private void FixedUpdate()
